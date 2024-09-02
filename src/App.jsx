824668bc20ec26e4f468fdcd72ef2928222bdccb
MyPc <brianw119@outlook.com>
2024-09-02 21:54:13 +0100

    Basic skeleton of project created

--- src/App.jsx
+++ src/App.jsx
@@ -1,2 +1,147 @@
-import React from 'react'
+
+import './myIndex.css'
+import {useRef} from 'react';
+import React from 'react';
+
+
+function CreateButton({buttonClr = "green", id,textClr = "white", type}){
+    const buttonStyle = {
+        color: textClr,
+        backgroundColor: buttonClr,
+    }
+    const handleClick = () => {
+        alert("clicked");
+    }
+    return (
+        <div>
+            <button style={buttonStyle} id="submit"  type="submit">Submit</button>
+        </div>
+    );
+};
+
+function CreateInput({type, ref, placeholder, htmlFor, className, id, name}){
+    return (
+        <div>
+            <label htmlFor={htmlFor}></label>
+            <input id={id} ref={ref} type={type} name={name} placeholder={placeholder} className={className}/>
+        </div>
+    );
+}
+
+export default function RenderData(){
+      const name1 = useRef(null);
+      const handleSubmit = (event) => {
+        event.preventDefault();
+        console.log(name1.current);
+    };
+    return(
+        <form onSubmit={handleSubmit} id="outerBox">
+            <div id="heading">My CV</div>
+         <div className="box">
+            <div id="section1" className="title">Personal Details</div>
+              <div id="name">
+                <CreateInput
+                  htmlFor="name1"
+                  className="personalDtls inputField"
+                  placeholder="First Name"
+                  id="name1"
+                  ref={name1}
+               />
+               <CreateInput
+                  htmlFor="name2"
+                  className="personalDtls inputField"
+                  placeholder="Last Name"
+                  id="name2"
+               />
+           </div>
+           <CreateInput 
+             htmlFor="address"
+             id="address"
+             placeholder="Your Address"
+             className="inputField"
+           />
+           <CreateInput
+              htmlFor="email"
+              type="email"
+              placeholder="Enter Your email"
+              id="email"
+              className="inputField"
+           />
+
+           <CreateInput
+              htmlFor="phoneNumber"
+              type="number"
+              className="inputField"
+              placeholder="Your Phone number"
+              id="phoneNumber"
+           />
+
+           <div className="blank"></div>
+           <div className="title">Employment</div>
+
+           <div className="subtitle">Current Employer:</div>
+           <CreateInput 
+              htmlFor="employment1"
+              className="jobs"
+              placeholder="List Company Name, employment date and job description"
+              id="employment1"
+           />
+
+           <div className="subtitle">Previous Employer:</div>
+           <CreateInput 
+              htmlFor="employment2"
+              className="jobs"
+              placeholder="List Company Name, employment date and job description"
+              id="employment2"
+           />
+
+           <div className="subtitle">Previous Employer:</div>
+           <CreateInput 
+              htmlFor="employment3"
+              className="jobs"
+              placeholder="List Company Name, employment date and job description"
+              id="employment3"
+           />
+        
+           <div className="blank"></div>
+           
+           <div id="education">Education</div>
+              <CreateInput
+                htmlFor="school1"
+                className="schooling"
+                placeholder="Qualification 1"
+                id="school1"
+              />
+              <CreateInput
+                htmlFor="school2"
+                className="schooling"
+                placeholder="Qualification 2"
+                 id="school2"
+              />
+             <CreateInput
+                htmlFor="school3"
+                className="schooling"
+                placeholder="Qualification 3"
+                 id="school3"
+            />
+            <CreateInput
+             htmlFor="school4"
+                className="schooling"
+                placeholder="Additional Qualification"
+                 id="school4"
+            />
+
+            <div className="blank"></div>
+
+            <div id="education">Further Information</div>
+            <CreateInput
+                className="additionalInfo"
+            />
+           <div className="blank"></div>
+           <CreateButton onClick={handleSubmit} type="submit"/>  
+          </div>
+          <div id="emptySpace">.</div>
+        </form>
+    );
+};
 
---
+++ src/App2.jsx
@@ -0,0 +1,30 @@
+
+import './myIndex.css';
+import RenderData from './index.jsx';
+import {useState} from 'react';
+
+
+function CreateDiv({id, className, innerText}){
+    return(
+       <div id={id} className={className}>{innerText}</div>
+    );
+};
+
+export default function CV(){
+    return(
+        <div id="outline">
+          <CreateDiv id="fullName"/>
+          <CreateDiv id="fullAddress"/>
+          <CreateDiv id="emailAddress"/>
+          <CreateDiv id="contactNumber"/>
+          <CreateDiv className="line"/>
+          <CreateDiv className="sectionTitle" innerText="Work Experience"/>
+          <CreateDiv className="experience" id="currentRole" innerText=""/>
+          <CreateDiv className="experience" id="previousRole1" innerText=""/>
+          <CreateDiv className="experience" id="previousRole2" innerText=""/>
+          <CreateDiv className="line"/>
+          <CreateDiv className="sectionTitle" innerText="More Information"/>
+          <CreateDiv className="experience" id="moreDetails" innerText=""/>
+        </div>  
+    );
+};
